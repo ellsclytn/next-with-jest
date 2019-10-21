399927ebc59ce49d5cd54a499fbe3f8c633d3786
Ellis Clayton <ellis@ellisclayton.info>
2019-10-21 15:05:39 +1100

    Add Dynamic component
    In order to reproduce the error in Jest:
    
    ```
    TypeError: require.resolveWeak is not a function
    ```

---
+++ components/MyTestComponent/index.js
@@ -0,0 +1,3 @@
+export const MyTestComponent = () => (
+  <p>Hi, I should by dynamically loaded</p>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import dynamic from 'next/dynamic'
+
+const DynamicMyTestComponent = dynamic(() => import('../components/MyTestComponent').then(({ MyTestComponent }) => MyTestComponent ))
+
 export default () => (
   <div>
     <style jsx>{`
@@ -6,5 +10,6 @@ export default () => (
       }
     `}</style>
     <p>Hello World!</p>
+    <DynamicMyTestComponent />
   </div>
 )
